5d29eb18299510863ddb7fde8e0a0f4ffaeca080
Mitchel Sandidge <masandidge12@gmail.com>
2020-06-18 11:08:34 -0500

    practicing making comoinents with react

--- src/index.js
+++ src/index.js
@@ -3,24 +3,58 @@ import React from "react";
 import ReactDOM from 'react-dom';
 
 
+// ############## JSX ##############
+// // Create a react component
+// const App =  () => {
+//
+//     const buttonText = 'Click me';
+//
+//     return (
+//         <div style={{backgroundColor: 'lightyellow', height: '500px'}}>
+//             <h1 style={{color: 'rebeccapurple'}}>Hello React!</h1>
+//             <label className="label" htmlFor="name">Enter name:</label>
+//             <input id="name" type="text"/>
+//             <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText}</button>
+//         </div>
+//     );
+// };
+//
+// //  Take the react component and show it on the screen
+//
+// ReactDOM.render(
+//     <App />,
+//     document.querySelector('#root')
+//
+//
+// );
+// ############## JSX ##############
 
-// Create a react component
-const App =  () => {
+
+
+const App  = () => {
     return (
-        <div style={{backgroundColor: 'lightyellow', height: '500px'}}>
-            <h1 style={{color: 'rebeccapurple'}}>Hello React!</h1>
-            <label class="label" for="name">Enter name:</label>
-            <input id="name" type="text"/>
-            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+        <div className={'ui container comments'}>
+            <div className="comment">
+                <a href="/" className={'avatar'}>
+                    <img alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className={'author'}>
+                        Sam
+                    </a>
+                    <div className={'metadata'}>
+                        <span className={'date'}>Today at 6:00</span>
+                    </div>
+                    <div className="text">
+                        Nice blog post!
+                    </div>
+                </div>
+            </div>
         </div>
-    );
+    )
 };
 
-//  Take the react component and show it on the screen
-
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+ReactDOM.render(<App/>, document.querySelector('#root'))
 
 
-);
+
